de2715e9a1fbf18eb432e583468d6acbb4cfe8e8
Evan Maloney <emaloney@gilt.com>
2016-07-22 13:14:10 -0400

    update README to note swift2.3 and swift3 branches, plus Travis build status badges

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-	name: "CleanroomASL",
-	dependencies: [
-		.Package(url: "https://github.com/emaloney/AppleSystemLogSwiftPackage", majorVersion: 1)
-	]
+    name: "CleanroomASL"
 )
