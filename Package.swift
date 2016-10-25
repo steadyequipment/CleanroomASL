003f14a723c02e33a337a527fd5c7eac0fa1f5bc
Ian G <yo.ian.g@gmail.com>
2016-10-25 14:40:00 -0400

    use steadyequipment’s fork of ASLSwift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "CleanroomASL",
 	dependencies: [
-		.Package(url: "https://github.com/emaloney/AppleSystemLogSwiftPackage", majorVersion: 1)
+		.Package(url: "https://github.com/steadyequipment/AppleSystemLogSwiftPackage", majorVersion: 1)
 	]
 )
